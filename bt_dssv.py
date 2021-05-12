9fa6e23ae399a8dfd2d0b12a4f477ab769faf7e7
toan27062002 <72190979+toan27062002@users.noreply.github.com>
2021-05-12 13:50:20 +0700

    Add files via upload

---
+++ bt_dssv.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created 
+
+@author: acer
+"""
+
+import pandas as pd
+import csv
+import random
+
+file = pd.read_csv('C:/Users/acer/Downloads/danh-sach-sinh-vien.csv')
+print(file.head(14))
+print("-"*34)
+
+#Chọn ngẫu nhiên 7 sv trong ds
+files = file.sample(n=7)
+#Lưu vào file csv mới tên DSSVmoi.csv
+files.to_csv(r'C:/Users/acer/Downloads/DSSV_moi.csv', index = None, header=True)
+print(files)
